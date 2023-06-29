88e686c9ddd6d960daf2b2102f2276dffc5f213f
Rutvik-Kalathiya <rutvik.kalathiya@purpleleafstrategy.com>
2023-06-29 17:47:34 +0200

    Displaying platform icon

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import { Card, CardBody } from '@chakra-ui/card';
 import { Game } from '../hooks/useGames';
 import { Image } from '@chakra-ui/image';
-import { Heading } from '@chakra-ui/layout';
+import { Heading, Text } from '@chakra-ui/layout';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
     game: Game;
@@ -13,6 +14,7 @@ const GameCard = ({game}: Props) => {
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
             </CardBody>
         </Card>
      );
